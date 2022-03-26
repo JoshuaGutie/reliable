82a580024cd685a2ce1717238100717a8aff22f2
Josh <52718132+JoshuaGutie@users.noreply.github.com>
2022-03-26 11:04:42 -0500

    contact started

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import "./css/contactus.css";
-import Footer from "./Footer"
-
+import Footer from "./Footer";
+import blue from "./images/blue.png"
 
 class Contactus extends Component {
   state = {};
   render() {
     return (
       <div className="containerc">
-        <div className="logoContainer">
-
-        </div>
+        <div className="logoContainer"></div>
         <div className="banner">
-<h1>Get in touch</h1>
-<p>Want to get in touch? We'd love to hear from you. Here's how you can reach us.</p>
+          <h1>Get in touch</h1>
+          <p>
+            Want to get in touch? We'd love to hear from you. Here's how you can
+            reach us.
+          </p>
         </div>
-          <Footer />
+        <div className="underB">
+          
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/cards/Call.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Call(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Call;
